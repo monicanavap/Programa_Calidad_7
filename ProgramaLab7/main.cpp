928119f9ca263708426d70532ec9d4a2497b313f
monicaanavap <31547577+monicaanavap@users.noreply.github.com>
2020-05-15 18:31:48 -0500

    Initial Commit

---
+++ ProgramaLab7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProgramaLab7
+//
+//  Created by Monica Nava on 5/15/20.
+//  Copyright © 2020 Monica Nava. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
